dd91c7fc00e3b60f0bdfbf699db4920d8806d0b1
Luarkenbb <lamchunpang17@gmail.com>
2018-10-13 20:48:04 +0800

    newnew
    123

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
   <link rel="stylesheet" type="text/css" media="screen" href="css/bg.css"/>
   <link rel="stylesheet" type="text/css" media="screen" href="css/table.css"/>
+  <link rel="stylesheet" type="text/css" media="screen" href="css/input.css"/>
   <style>
   table tr td{
     padding: 2px;
@@ -21,12 +22,12 @@
     <!-- values: userType username userpwd-->
     <form method="post" action="login.php">
       <table>
-      <tr><th colspan='2'>Login</th></tr>
+      <tr><th colspan='2'>Welcome!</th></tr>
       <tr><td>User type:</td><td>
         <input name="userType" list="type" type="text"/>
       </td></tr>
-      <tr><td>User name:</td><td><input name='username' type='text' /></td></tr>
-      <tr><td>Password:</td><td><input name='userpwd' type='password' /></td></tr>
+      <tr><td colspan="2"><input class="input" type="text"  name ="username" required="required" placeholder="Please enter username"/></td></tr>
+      <tr><td colspan="2"><input class="input" type="password"  name= "userpwd" required="required" placeholder="Please enter Password"/></td></tr>
       <tr><td></td><td><input type='Submit' style='float: right;' /></td></tr>
       </table>
     </form>

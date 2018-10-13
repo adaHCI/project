4d5b09480b9ba7ec488a1e7bad1c5e3b03490150
Luarkenbb <lamchunpang17@gmail.com>
2018-10-13 17:25:44 +0800

    123

--- index.php
+++ index.php
@@ -3,11 +3,17 @@
 
 <head>
   <link rel="stylesheet" type="text/css" media="screen" href="css/bg.css"/>
+  <link rel="stylesheet" type="text/css" media="screen" href="css/table.css"/>
 </head>
 
 <body>
   <div class="contentBorder">
-    abc888
+    <table class="myTable">
+      <tr><th>1</th><th>2</th></tr>
+      <tr><td>222</td><td>444</td></tr>
+      <tr><td>33333</td><td>66666</td></tr>
+    </table>
   </div>
+
 </body>
 </html>

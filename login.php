5b05467656a09eda0d91522857d0f273a6712c04
Luarkenbb <lamchunpang17@gmail.com>
2018-10-14 16:30:39 +0800

    test for login
    login

--- login.php
+++ login.php
@@ -26,17 +26,30 @@
       /*valid input*/
       require_once('dbsql/conn.php');
       $table = "";
+      $url = "";
       if($loginType == "staff_teaching"){
         $table = "teachingStaff";
+        $url = "teachingStaff/home.php";
       }else if($loginType == "staff_nonTeaching"){
         $table = "non_teachingStaff";
+        $url = "non_teachingStaff/home.php";
       }else if($loginType == "student"){
         $table = "students";
+        $url = "student/home.php";
       }else if($loginType == "alumni"){
         $table = "alumni";
+        $url = "alumni/home.php";
       }
-      $sql = $conn->prepare("SELECT ? where userName = ? and userPwd = ?");
-      $sql->bind_param($table,$loginName,$loginPwd);
+      $sql = "SELECT * FROM '$table' where userName = '$loginName' and userPwd = '$loginPwd';";
+      $rs = mysqli_query($conn,$sql);
+      /*successfully login*/
+      if(mysqli_num_rows($rs) == 1){
+        echo "<div class='contentBorder'>";
+        echo "<p>Please re-enter the information</p>";
+        echo "<meta http-equiv=\"refresh\" content=\"3; URL='$url'\"/> ";
+        echo "</div>";
+      }
+
 		?>
 	</body>
 </html>

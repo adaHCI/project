1a7d09585d6e6e92aefa9e71cbe4236af367f299
Luarkenbb <lamchunpang17@gmail.com>
2018-10-14 16:49:58 +0800

    Update alumni/home.php
    123

--- alumni/home.php
+++ alumni/home.php
@@ -1,20 +1,15 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
-<style type="text/css">
-<!--
-.STYLE1 {
-	font-size: xx-large;
-	color: #CC3399;
-}
--->
-</style>
+<title>alumni</title>
+<link rel="stylesheet" type="text/css" media="screen" href="../css/bg.css"/>
+<link rel="stylesheet" type="text/css" media="screen" href="../css/topbar.css"/>
 </head>
 
 <body>
-<span class="STYLE1">Welcome
-alumni</span>
+<ul>
+	<li><a href="">abc</a></li>
+	<li><a href="">def</a></li>
+</ul>
 </body>
 </html>

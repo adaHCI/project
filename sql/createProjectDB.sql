ba332f2b9063d535d59d4f643f6d3aecb70dc9d0
sanchisau <43124734+sanchisau@users.noreply.github.com>
2018-10-17 17:14:46 +0800

    new change
    new change

---
+++ sql/createProjectDB.sql
@@ -0,0 +1,48 @@
+
+DROP TABLE IF EXISTS `alumni`;
+
+CREATE TABLE `alumni` (
+  `userID` int(11) NOT NULL,
+  `userName` varchar(20) NOT NULL,
+  `userPwd` varchar(20) NOT NULL,
+  `userEmail` varchar(20) NOT NULL,
+  `userPhone` int(20) NOT NULL,
+  `isFirstLogin` BIT DEFAULT 1,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `non_teachingStaff`;
+
+CREATE TABLE `non_teachingStaff` (
+  `userID` int(11) NOT NULL,
+  `userName` varchar(20) NOT NULL,
+  `userPwd` varchar(20) NOT NULL,
+  `userEmail` varchar(20) NOT NULL,
+  `userPhone` int(20) NOT NULL,
+  `isFirstLogin` BIT DEFAULT 1,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `students`;
+
+CREATE TABLE `students` (
+  `userID` int(11) NOT NULL,
+  `userName` varchar(20) NOT NULL,
+  `userPwd` varchar(20) NOT NULL,
+  `userEmail` varchar(20) NOT NULL,
+  `userPhone` int(20) NOT NULL,
+  `isFirstLogin` BIT DEFAULT 1,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `teachingStaff`;
+
+CREATE TABLE `teachingStaff` (
+  `userID` int(11) NOT NULL,
+  `userName` varchar(20) NOT NULL,
+  `userPwd` varchar(20) NOT NULL,
+  `userEmail` varchar(20) NOT NULL,
+  `userPhone` int(20) NOT NULL,
+  `isFirstLogin` BIT DEFAULT 1,
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;

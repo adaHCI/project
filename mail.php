0becb5c93a60b1e4a03ded76118d19f72b4c1f98
sanchisau <43124734+sanchisau@users.noreply.github.com>
2018-11-22 05:34:11 +0800

    1111111111111
    11111111

--- mail.php
+++ mail.php
@@ -1,3 +1,4 @@
+
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -6,13 +7,16 @@
 <?php
   require_once('dbsql/conn.php');
 $email=$_REQUEST["userEmail"];
-$sql = "select * from forget where userEmail='$email'";
+$a=rand(100000,999999);
+$sqla = "UPDATE user SET isFirstLogin = 1, userPwd = '$a' WHERE userEmail = '$email'";
+    mysqli_query($conn,$sqla);
+$sql = "select * from user where userEmail='$email'";
 $query = mysqli_query($conn, $sql);
 $row=mysqli_fetch_array($query);
 
 require 'PHPMailer-master/PHPMailerAutoload.php';
 $mail = new PHPMailer();
-  
+
   //Enable SMTP debugging.
   $mail->SMTPDebug = 0;
   //Set PHPMailer to use SMTP.
@@ -35,16 +39,16 @@ $mail = new PHPMailer();
   $mail->SMTPSecure = "false";
   $mail->Port = 587;
   //Set TCP port to connect to
-  
+
   $mail->From = "1701859@gmail.com";
   $mail->FromName = "KB sam";
-  
+
   $mail->addAddress($row["userEmail"]);
-  
+
   $mail->isHTML(true);
- 
+
   $mail->Subject = "It is forget password!";
-  $mail->Body = "<i>HI,<br />This is your password:</i>".$row["userPwd"]."<br />==============================================<br />go http://localhost/project/";
+  $mail->Body = "<i>HI,<br />Your password has been reset. Please use this temporary password to change your password:</i>".$row["userPwd"]."<br />==============================================<br />go http://localhost/project/";
   $mail->AltBody = "This is the plain text version of the email content <a href=''>";
   ?>
   </head>
@@ -62,4 +66,3 @@ $mail = new PHPMailer();
    echo "</div>";
   }
   ?>
-  
